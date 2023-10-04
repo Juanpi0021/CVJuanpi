e6097b980989f25f6152151b4568a08782ac27df
Juanpi0021 <juan.ortiz4968@alumnos.udg.mx>
2023-10-04 14:26:01 -0600

    Carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,16 +1,24 @@
-let sliderInner=document.querySelector(".slider--inner");
+const carrusel = document.getElementById("carrusel");
+const imagenes = document.querySelectorAll(".imagen-carrusel");
+const botonAnterior = document.getElementById("anterior");
+const botonSiguiente = document.getElementById("siguiente");
 
-let images = sliderInner.querySelectorAll("img")
+let index = 0;
 
-let index=0;
-
-setInterval(function(){
-    let percentage=index*-100;
-    sliderInner.style.transform="translateX("+percentage + "%)";
-    index++;
-    if(index>images.length-1)
-    {
-        index=0;
+function cambiarImagen(direccion) {
+    index += direccion;
+    if (index < 0) {
+        index = imagenes.length - 1;
+    } else if (index >= imagenes.length) {
+        index = 0;
     }
-    
-},1000);
+
+    const desplazamiento = -index * 300; // Ancho de cada imagen
+    carrusel.style.transform = `translateX(${desplazamiento}px)`;
+}
+
+botonAnterior.addEventListener("click", () => cambiarImagen(-1));
+botonSiguiente.addEventListener("click", () => cambiarImagen(1));
+
+// Iniciar con la primera imagen
+cambiarImagen(0);
